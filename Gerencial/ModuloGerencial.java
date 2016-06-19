2fa44d744bd27a17d4e8eb0df35400fa1fe4a38f
João Pedro Pianta <jppianta83@gmail.com>
2016-06-19 15:38:52 -0300

    Add files via upload

--- Gerencial/ModuloGerencial.java
+++ Gerencial/ModuloGerencial.java
@@ -1,1 +1,35 @@
-
+public class ModuloGerencial {
+	private boolean carregar() {
+		//ver se existe um arquivo, se n existir criar .se e inicializar lista d tickets, se n, carregar .se e salvar lista d tickets.
+		File in = new File("tickets.se");
+		if (in.exists()) {
+			try {
+				FileInputStream fis = new FileInputStream("tickets.se");
+				ObjectInputStream ois = new ObjectInputStream(fis);
+				tickets = (List<Ticket>) ois.readObject();
+				fis.close();
+				ois.close();
+			} catch (IOException i) {
+				i.printStackTrace();
+				return false;
+			} catch(ClassNotFoundException c) {
+				System.out.println("classe ticket n existe");
+				c.printStackTrace();
+				return false;
+			}
+		} else {
+			try {
+				FileOutputStream fis = new FileOutputStream("tickets.se");
+				ObjectOutputStream ois = new ObjectOutputStream(fis);
+				ois.writeObject(tickets);
+				ois.close();
+				fis.close();
+			} catch (IOException i) {
+				i.printStackTrace();
+				return false;
+			}
+		}
+		in.close();
+		
+	}
+}
---
+++ Gerencial/Ticket.java
@@ -0,0 +1,37 @@
+import java.time.LocalTime;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ticket {
+
+	private String idParquimetro;
+	private String endereco;
+	private String serial;
+	private String pagamento;
+	private LocalTime emissao;
+	private LocalTime validade;
+	
+	private static List<Ticket> tickets = new LinkedList<Ticket>();
+	private static TicketDao tk;
+	
+	public boolean salvarRelatorio()
+	{
+		boolean resposta = tk.salvarRelatorio(tickets) ;
+	
+	}	
+	public Ticket(String idParquimetro, String endereco, String serial, LocalTime emissao, LocalTime validade, String fpagamento) {
+		this.idParquimetro = idParquimetro;
+		this.endereco = endereco;
+		this.serial = serial;
+		this.emissao = emissao;
+		this.validade = validade;
+		pagamento=fpagamento;
+	}
+	public String toString()
+	{
+		return idParquimetro + "-" + serial + " Endereço: " + endereco + " Emissão: " + emissao + " Validade: " + validade;
+	}
+
+	
+}
+
