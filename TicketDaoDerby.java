f19a50b51c1ec3629aa52e9a262ac5ce4e096da9
João Pedro Pianta <jppianta83@gmail.com>
2016-06-19 15:21:05 -0300

    Add files via upload

---
+++ TicketDaoDerby.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import java.util.LinkedList;
+import java.io.*;
+
+public class TicketDaoDerby {
+	private List<Ticket> tickets=null;
+	
+	
+	private boolean carregar() {
+		//ver se existe um arquivo, se n existir criar .se e inicializar lista d tickets, se n, carregar .se e salvar lista d tickets.
+		File in = new File("tickets.se");
+		if (in.exists()) {
+			try {
+				FileInputStream fis = new FileInputStream("tickets.se");
+				ObjectInputStream ois = new ObjectInputStream(fis);
+				tickets = (List<Ticket>) ois.readObject();
+				fis.close();
+				ois.close();
+			} catch (IOException i) {
+				i.printStackTrace();
+				return false;
+			} catch(ClassNotFoundException c) {
+				System.out.println("classe ticket n existe");
+				c.printStackTrace();
+				return false;
+			}
+		} else {
+			FileOutputStream fis = new FileOutputStream("tickets.se");
+			ObjectOutputStream ois = new ObjectOutputStream(fis);
+			ois.writeObject(tickets);
+			ois.close();
+			fis.close();
+		}
+		in.close();
+		
+	}
+	
+}
