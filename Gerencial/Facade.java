124234288e38012527a2b807cb37a874ddb1f779
CidRed <henry.nunes@outlook.com>
2016-06-20 13:23:40 -0300

    Add files via upload

---
+++ Gerencial/Facade.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Facade{
+
+public List<String> logParquimetrosDia(String dia)
+{
+
+}
+public List<String> logParquimetrosMes(String mes)
+{
+
+}
+public List<String> relatorioParquimetroMes(String Parquimetro) 
+{
+
+}
+public List<String> relatorioParquimetroAno(String Parquimetro) 
+{
+
+}
+
+
+
+}
