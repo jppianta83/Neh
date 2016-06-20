c90fa3dc4dd82d3e88272bc3bf2d7c17bcf225ce
leandrodeopereira <leandro.pereira.002@acad.pucrs.br>
2016-06-20 12:52:38 -0300

    Add files via upload

--- Gerencial/Ticket.java
+++ Gerencial/Ticket.java
@@ -1,6 +1,8 @@
+import java.math.BigDecimal;
 import java.time.LocalTime;
 import java.util.LinkedList;
 import java.util.List;
+import java.time.LocalDate;
 
 public class Ticket {
 
@@ -8,30 +10,59 @@ public class Ticket {
 	private String endereco;
 	private String serial;
 	private String pagamento;
+	private BigDecimal valor;
 	private LocalTime emissao;
 	private LocalTime validade;
+	private LocalDate data;
 	
-	private static List<Ticket> tickets = new LinkedList<Ticket>();
-	private static TicketDao tk;
-	
-	public boolean salvarRelatorio()
-	{
-		boolean resposta = tk.salvarRelatorio(tickets) ;
-	
-	}	
-	public Ticket(String idParquimetro, String endereco, String serial, LocalTime emissao, LocalTime validade, String fpagamento) {
+	public Ticket(String idParquimetro, String endereco, String serial, LocalTime emissao, LocalTime validade, String fpagamento, BigDecimal tarf) {
 		this.idParquimetro = idParquimetro;
 		this.endereco = endereco;
 		this.serial = serial;
 		this.emissao = emissao;
 		this.validade = validade;
-		pagamento=fpagamento;
+		this.pagamento = fpagamento;
+		this.valor = tarf;
+		data = LocalDate.now();
 	}
+	
+	public LocalDate getData()
+	{
+		return data;
+	}
+	
 	public String toString()
 	{
-		return idParquimetro + "-" + serial + " Endereço: " + endereco + " Emissão: " + emissao + " Validade: " + validade;
+		return idParquimetro + "-" + serial + " Endere�o: " + endereco + " Emissão: " + emissao + " Validade: " + validade + " Pagamento: " + pagamento + " Data: " + data;
+	}
+	
+	public String getIdParquimetro() {
+		return idParquimetro;
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	
+	public String getSerial() {
+		return serial;
+	}
+	
+	public String getPagamento() {
+		return pagamento;
+	}
+	
+	public LocalTime getEmissao() {
+		return emissao;
+	}
+	
+	public LocalTime getValidade() {
+		return validade;
+	}
+	public BigDecimal valor()
+	{
+		return valor;
 	}
 
-	
 }
 
